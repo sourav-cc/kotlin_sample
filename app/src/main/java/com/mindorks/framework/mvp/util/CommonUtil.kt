dc665420c35cc7155801b8797b81ac484929501d
sourav <sourav@github.com>
2018-02-23 18:27:12 +0530

    initial commit

---
+++ app/src/main/java/com/mindorks/framework/mvp/util/CommonUtil.kt
@@ -0,0 +1,27 @@
+package com.mindorks.framework.mvp.util
+
+import android.app.ProgressDialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import com.mindorks.framework.mvp.R
+
+/**
+ * Created by jyotidubey on 11/01/18.
+ */
+object CommonUtil {
+
+    fun showLoadingDialog(context: Context?): ProgressDialog {
+        val progressDialog = ProgressDialog(context)
+        progressDialog.let {
+            it.show()
+            it.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            it.setContentView(R.layout.progress_dialog)
+            it.isIndeterminate = true
+            it.setCancelable(false)
+            it.setCanceledOnTouchOutside(false)
+            return it
+        }
+    }
+
+}
